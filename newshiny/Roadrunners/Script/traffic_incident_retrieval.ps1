62de6394612d95013103606255e6c8465cb0a448
tanmingkwang <mktan.2015@sis.smu.edu.sg>
2018-04-13 00:15:53 +0800

    Added Script that calls LTA API
    added the script that was used to perform the data collection

---
+++ newshiny/Roadrunners/Script/traffic_incident_retrieval.ps1
@@ -0,0 +1,10 @@
+﻿$headers = @{}
+$headers.Add("AccountKey","ix23KZcYSeiEbkqWoH2mGQ==")
+$headers.Add("Accept","application/json")
+$uri = 'http://datamall2.mytransport.sg/ltaodataservice/TrafficIncidents'
+
+$timer = (Get-Date -Format yyy-mm-dd-hhmm)
+$filepath = "C:\temp\" + $timer + ".csv"
+
+$data = Invoke-RestMethod -Method Get -Uri $uri -Headers $headers
+$data.value | Export-Csv -Append -Path $filepath
